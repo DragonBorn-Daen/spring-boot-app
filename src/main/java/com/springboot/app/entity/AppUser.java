10bb14bd30c233d5cb61d6b06ad983d2e65da69e
xtreme-uz <rustambekov.mail.box@gmail.com>
2020-10-05 09:22:53 +0500

    crud operation

---
+++ src/main/java/com/springboot/app/entity/AppUser.java
@@ -0,0 +1,41 @@
+package com.springboot.app.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class AppUser extends BaseEntity {
+
+    private String username;
+
+    private String password;
+
+    public AppUser() {
+    }
+
+    public AppUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public AppUser(Long id, String username, String password) {
+        super(id);
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
